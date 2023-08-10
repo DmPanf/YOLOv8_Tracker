210c252a2b918bf0a912c491dc3926303949c292
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-10 04:09:30 +0300

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,10 @@
+import time
+
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        return result, execution_time
+    return wrapper
