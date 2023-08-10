4a9137852f974afa2aab7d1c1bbb0b4bb62a8fa5
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-10 04:10:17 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,10 +1,13 @@
 import time
+from functools import wraps
 
 def timer_decorator(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        execution_time = end_time - start_time
-        return result, execution_time
+        print(f"Function {func.__name__} took {(end_time - start_time):.2f}s to execute.")
+        return result
     return wrapper
+
